d4a69c00cdbd708d14cf0d849ef7d0662e73b42e
grzeznx2 <grzeznx2@gmail.com>
2021-11-18 19:43:27 +0100

    create IERC20 to do list

---
+++ contracts/IERC20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+/*
+TO DO:
+totalSupply()
+
+balanceOf(account)
+
+transfer(recipient, amount)
+
+allowance(owner, spender)
+
+approve(spender, amount)
+
+transferFrom(sender, recipient, amount)
+ */
+
+
+interface IERC20 {
+    
+}
