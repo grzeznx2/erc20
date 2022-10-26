e0294384696b20b76a197dfd294b4cf4cf0a40be
grzeznx2 <grzeznx2@gmail.com>
2022-10-26 16:37:55 +0200

    Add _spendAllowance method

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -114,4 +114,16 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
 
         emit Transfer(account, address(0), amount);
     }
+
+    function _spendAllowance(address owner, address spender, uint256 amount) internal virtual {
+        uint256 currentAllowance = allowance(owner, spender);
+
+        if(currentAllowance != type(uint256).max){
+            require(currentAllowance >= amount, "ERC20: insufficient allowance");
+
+            unchecked {
+                _approve(owner, spender, amount);
+            }
+        }
+    }
 }
