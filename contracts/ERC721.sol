fed010e87ec6ba56fa659af8db1bcab517e09d4f
grzeznx2 <grzeznx2@gmail.com>
2022-11-26 21:37:41 +0100

    Start working on ERC721 contract

---
+++ contracts/ERC721.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./IERC721.sol";
+
+contract ERC721 is IERC721 {
+
+    string private _name;
+    string private _symbol;
+
+    constructor(){}
+}
