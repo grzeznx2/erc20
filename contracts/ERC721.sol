dd9c3541d7620e34931eb766336bef116ba76538
grzeznx2 <grzeznx2@gmail.com>
2022-11-27 19:54:24 +0100

    Add ERC721 properties

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -9,5 +9,13 @@ contract ERC721 is IERC721 {
     string private _name;
     string private _symbol;
 
-    constructor(){}
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => address) private _owners;
+    mapping(uint256 => address) private _approvals;
+    mapping(address => mapping(address => bool)) private _operators;
+
+    constructor(string memory name_, string memory symbol_){
+        _name = name_;
+        _symbol = symbol_;
+    }
 }
